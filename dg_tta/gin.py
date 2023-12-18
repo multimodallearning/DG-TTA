35688381ca46cdd3433e00d3ba1125fe45b1e52c
Christian Weihsbach <christian.weihsbach@uni-luebeck.de>
2023-12-18 17:58:01 +0100

    Add GIN copyright info

--- dg_tta/gin.py
+++ dg_tta/gin.py
@@ -1,3 +1,31 @@
+# MIT License
+
+# Copyright (c) 2022 Cheng
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# GIN Code based on:
+# Ouyang, C., Chen, C., Li, S., Li, Z., Qin, C., Bai, W., & Rueckert, D. (2022). Causality-inspired single-source domain generalization for medical image segmentation. IEEE Transactions on Medical Imaging, 42(4), 1095-1106.
+# DOI: 10.1109/TMI.2022.3224067
+# https://github.com/cheng-01037/Causality-Medical-Image-Domain-Generalization
+# https://ieeexplore.ieee.org/document/9961940
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
